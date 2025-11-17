2544329575b9282eeef504c4fe71bce49cca39e9
IRU ALBINO <130148805+IRUALBINO@users.noreply.github.com>
2025-11-17 10:10:54 +0530

    Update HelloWorld.java

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,17 @@
+import com.sun.net.httpserver.HttpServer;
+import java.net.InetSocketAddress;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", exchange -> {
+            String response = "Hello from Java App on EC2!";
+            exchange.sendResponseHeaders(200, response.length());
+            exchange.getResponseBody().write(response.getBytes());
+            exchange.close();
+        });
+        server.start();
+
+        System.out.println("Server started on port 8080");
+    }
+}
